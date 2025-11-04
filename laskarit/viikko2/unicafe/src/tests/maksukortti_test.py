c5efbf0f2e1ccb6933d5e3108be72f93a2371d3c
Jukka Kuusto <jukka.kuusto@gmail.com>
2025-11-04 14:03:58 +0200

    Complete week 2 exercises 6-7

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,33 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_kortin_saldo_alussa_oikein(self):
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+    
+    def test_rahan_lataaminen_kasvattaa_saldoa_oikein(self):
+        self.maksukortti.lataa_rahaa(1000)
+
+        self.assertEqual(self.maksukortti.saldo_euroina(), 20.0)
+
+    def test_saldo_vahenee_oikein_jos_rahaa_on_tarpeeksi(self):
+        self.maksukortti.ota_rahaa(240)
+
+        self.assertEqual(self.maksukortti.saldo_euroina(), 7.6)
+
+    def test_saldo_ei_muutu_jos_rahaa_ei_ole_tarpeeksi(self):
+        maksukortti = Maksukortti(200)
+        maksukortti.ota_rahaa(240)
+
+        self.assertEqual(maksukortti.saldo_euroina(), 2.0)
+
+    def test_rahan_ottaminen_palauttaa_true_jos_rahat_riittavat(self):
+        palautusarvo = self.maksukortti.ota_rahaa(240)
+
+        self.assertEqual(palautusarvo, True)
+
+    def test_rahan_ottaminen_palauttaa_false_jos_rahat_eivat_riita(self):
+        maksukortti = Maksukortti(200)
+        palautusarvo = maksukortti.ota_rahaa(240)
+
+        self.assertEqual(palautusarvo, False)
